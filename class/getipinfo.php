81080eaf9061e0ca810a03a011c9e956ebe2de3c
liangwei <362253310@qq.com>
2016-06-12 01:18:32 +0800

    update

--- class/getipinfo.php
+++ class/getipinfo.php
@@ -1,6 +1,8 @@
 <?php
 /*
 *	获取ip地址信息类  by  liangwei
+*	为思科技术社区实现用户回帖自动根据ip返回归属地功能.
+*	案例：http://www.sikejishu.com/forum.php?mod=viewthread&tid=7177 
 */
 	header("Content-Type:text/html;charset=utf-8");
 	require_once( 'getarea.class.php');   //引入getarea函数
